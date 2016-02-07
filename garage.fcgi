648d507871b64f67186e450f55a86d6527d0690b
Nathan Jones <nathan@nathanpjones.com>
2016-02-07 11:27:00 -0800

    Upgrade to Python 3
    Switch from flup to flipflop

--- garage.fcgi
+++ garage.fcgi
@@ -1,6 +1,6 @@
 #!venv/bin/python
 
-from flup.server.fcgi import WSGIServer
+from flipflop import WSGIServer
 from garage import app
 
 if __name__ == '__main__':
--- garage.py
+++ garage.py
@@ -19,22 +19,22 @@ import collections
 class GroupWriteRotatingFileHandler(RotatingFileHandler):
     def __init__(self, filename, mode='a', maxBytes=0, backupCount=0, encoding=None):
         super(GroupWriteRotatingFileHandler, self).__init__(filename, mode, maxBytes, backupCount, encoding)
-        print 'calling ensure permissions'
+        print('calling ensure permissions')
         self.ensurePermissions()
 
     def doRollover(self):
         """
         Override base class method to make the new log file group writable.
         """
-        print 'executing base rollover'
+        print('executing base rollover')
         # Rotate the file first.
         RotatingFileHandler.doRollover(self)
 
-        print 'calling ensure permissions'
+        print('calling ensure permissions')
         self.ensurePermissions()
 
     def ensurePermissions(self):
-        print 'ensuring permissions'
+        print('ensuring permissions')
         # Make sure the group is garage_site
         uid = os.stat(self.baseFilename).st_uid
         gid = grp.getgrnam("garage_site").gr_gid
